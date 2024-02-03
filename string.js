42b657cc7b551afe52cabb1449817add00dbef99
SanowerHossenShanto <ssarker515@gmail.com>
2024-02-04 03:35:03 +0600

    string intro

---
+++ string.js
@@ -0,0 +1,45 @@
+ const subject="Computer Science";
+ const book="computer science";
+ if(subject==book){
+    console.log("I Kopamu in the Exam")
+ }
+ else{
+    console.log("I Kope Khamu in the Exam!")
+ }
+ if(subject. toLocaleLowerCase()==book.toLocaleLowerCase()){
+    console.log("I Kopamu in the Exam")
+ }
+ else{
+    console.log("I Kope Khamu in the Exam!")
+ }
+
+
+ //-----------------------------
+ //----------------------------
+ //----------------------------
+ const village="Tangail";
+ const town="  Tangail";
+ if (village==town){
+    console.log("I go to Home")
+
+ }
+ else{
+    console.log("I will go to Varsity")
+ }
+
+
+
+ //--------------------
+
+//  trim for remove white space
+
+
+ const village1="Tangail";
+ const town1="  Tangail";
+ if (village1.trim()==town1.trim()){
+    console.log("I go to Home")
+
+ }
+ else{
+    console.log("I will go to Varsity")
+ }
